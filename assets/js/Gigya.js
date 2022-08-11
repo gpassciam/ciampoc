cad57b494e3090df021b3ade375501e2e0cea552
ciampoc <104811739+gpassciam@users.noreply.github.com>
2022-08-11 12:51:07 +0530

    11082022

---
+++ assets/js/Gigya.js
@@ -0,0 +1,412 @@
+﻿
+function load() {
+    //Moesif CORS
+    
+        $("#header").load("Header.html");
+        $("#footer").load("Footer.html");
+}
+
+
+function SignInScreen(flag) {
+    
+    //load();
+    
+    sessionStorage.setItem("flag", flag);
+    customLangParams = {
+
+        this_field_is_required: 'Please enter %fieldname'
+    };
+    
+
+    if (flag == 'b2b') {
+        console.log(flag);
+        localStorage.setItem("485c9a75-2633-4f90-b48d-dd2b3a891d86", "Delegated Admin");
+        localStorage.setItem("cbdc0b70-6d69-4946-8443-060de6fbefbc", "Ordering");
+        localStorage.setItem("8de8cd93-103f-4277-a8df-e7a697d75d14", "Returns");
+        gigya.accounts.showScreenSet({
+            screenSet: 'Online_Medical-RegistrationLogin',
+            startScreen: 'gigya-login-screen',
+            customLang: customLangParams,
+            onHide: hide
+            
+        });
+        gigya.accounts.addEventHandlers({
+            onLogin: onb2bLogin
+
+        });
+
+    
+
+    }
+
+    if (flag == 'b2c') {
+        console.log(flag);
+        gigya.accounts.showScreenSet({
+            screenSet: 'Online_Medical-RegistrationLogin',
+            startScreen: 'gigya-login-screen',
+            customLang: customLangParams,
+            onHide: hide
+        });
+
+        gigya.accounts.addEventHandlers({
+            onLogin: onb2cLogin
+
+        });
+
+    }
+    
+    
+}
+function onb2bLogin(response) {
+    console.log("Onb2bLogin:" + JSON.stringify(response));
+    let UID = response.UID;
+    sessionStorage.setItem("CUID", UID);
+    var params = {
+        "UID": UID,
+        "include": "groups,profile"
+
+    }
+
+    gigya.accounts.getAccountInfo(params, { callback: onTb2bLogin });
+
+    function onTb2bLogin(response) {
+        console.log("OnTb2bLogin:" + JSON.stringify(response));
+        let errorCode = response.errorCode;
+        let UID = response.UID;
+        var key = "UID";
+        let DBName = "Users";
+        let Table = "Users_Info";
+        let Data = JSON.parse(JSON.stringify(response));
+        ProcessDB(DBName, Table, Data, key);
+       
+        getUID(DBName, Table, UID).then(function (SUID) {
+            const Data = SUID.split(",");
+            sessionStorage.setItem("SUID", Data[0]);
+            sessionStorage.setItem("SName", Data[1]);
+            //alert(Data[2]);
+            sessionStorage.setItem("SProvider", Data[2]);
+            sessionStorage.setItem("SOrgName", Data[3]);
+            var session_UID = sessionStorage.getItem("SUID");
+            var session_Name = sessionStorage.getItem("SName");
+            if ((session_UID == null || typeof session_UID == "undefined") && errorCode == 0) {
+
+
+                window.location = 'index.html';
+
+            }
+            //else if (sessionStorage.getItem("DACheck") == "Yes") {
+            //    opendelegateadmin(sessionStorage.getItem("OrgID"));
+            //    sessionStorage.clear();
+            //}
+            else {
+                //console.log(session_UID);
+                window.location = 'Landingb2b.html';
+            }
+        });
+    }
+}
+
+function onb2cLogin(response) {
+    //console.log("flag : " + flag);
+    console.log("Onb2cLogin:" + JSON.stringify(response));
+    let UID = response.UID;
+    var key = "UID";
+    let DBName = "Users";
+    let Table = "Users_Info";
+    ProcessDB(DBName, Table, response, key);
+    onTb2cLogin(response);
+    //gigya.accounts.showScreenSet({
+    //    screenSet: 'Online_Medical-RegistrationLogin',
+    //    startScreen: 'gigya-tfa-verification-screen',
+    //    customLang: customLangParams,
+    //    onAfterSubmit: onTb2cLogin,
+    //    //containerID: 'divsignin'
+    //});
+    
+
+
+    function onTb2cLogin(response) {
+
+        console.log("OnTb2cLogin:" + JSON.stringify(response));
+        let errorCode = response.errorCode;
+        getUID(DBName, Table, UID).then(function (SUID) {
+            const Data = SUID.split(",");
+            sessionStorage.setItem("SUID", Data[0]);
+            sessionStorage.setItem("SName", Data[1]);
+            sessionStorage.setItem("SProvider", Data[2]);
+            var session_UID = sessionStorage.getItem("SUID");
+            var session_Name = sessionStorage.getItem("SName");
+            if ((session_UID == null || typeof session_UID == "undefined") && errorCode == 0) {
+
+
+                window.location = 'index.html';
+
+            }
+            else {
+                //console.log(session_UID);
+                window.location = 'Landingb2c.html';
+            }
+        });
+    }
+}
+
+function Getdata(page,UID) {
+    //alert('heree');
+    //let UID = response.UID;
+    let DBName = "Users";
+    let Table = "Users_Info";
+    getUID(DBName, Table, UID).then(function (SUID) {
+        const Data = SUID.split(",");
+        sessionStorage.setItem("SUID", Data[0]);
+        sessionStorage.setItem("SName", Data[1]);
+        var session_UID = sessionStorage.getItem("SUID");
+        var session_Name = sessionStorage.getItem("SName");
+        if (session_UID == null || typeof session_UID == "undefined") {
+
+            
+                window.location = 'index.html';
+           
+        }
+        else {
+            //console.log(session_UID);
+            window.location = page;
+        }
+    });
+}
+function Store(flag)
+{
+    sessionStorage.setItem("flag", flag);
+
+}
+function SignUpScreen(flag) {
+    //load();
+    //let flag = localStorage.getItem("flag");
+    console.log(flag);
+    //window.location = "Register.html";
+    customLangParams = {
+
+        this_field_is_required: 'Please enter %fieldname'
+    };
+    if (flag == 'b2c') {
+        //document.getElementById("divsignup").style.display = "none";
+        gigya.accounts.showScreenSet({
+            screenSet: 'Online_Medical-RegistrationLogin',
+            startScreen: 'gigya-register-screen',
+            customLang: customLangParams,
+            onHide:hide
+            //containerID: 'divsignup'
+        });
+    }
+
+    if (flag == 'b2b') {
+        //document.getElementById("divsignup").style.display = "none";
+        gigya.accounts.showScreenSet({
+            screenSet: 'Online_Medical-OrganizationRegistration',
+            startScreen: 'gigya-org-register-screen',
+            customLang: customLangParams,
+            //containerID: 'divsignup'
+            onHide: hide
+        });
+
+    }
+}
+function ShowEditScreen() {
+
+    
+    gigya.accounts.showScreenSet({
+        screenSet: 'Online_Medical-ProfileUpdate',
+        startScreen: 'gigya-update-profile-screen',
+        //containerID: 'profile',
+        onAfterSubmit: Update
+
+    });
+
+}
+
+function hide() {
+
+    
+    if (sessionStorage.getItem("CUID") == null || typeof sessionStorage.getItem("CUID") == "undefined") {
+        
+        location.reload();
+    }
+
+    
+
+}
+
+function opendelegateadmin(orgid) {
+    //alert(orgid);
+    var params = {
+        "orgId": orgid
+    }
+    gigya.accounts.b2b.openDelegatedAdminLogin(params);
+}
+
+async function Update(response) {
+    //console.log("OnUpdate:" + JSON.stringify(response));
+    var params = {
+        "UID": sessionStorage.getItem("SUID"),
+        "include": "identities-active,identities-all,identities-global,loginIDs,emails,profile,data, password,lastLoginLocation, regSource,irank,rba,subscriptions,userInfo",
+        "extraProfileFields": "languages,address,phones, education, honors, publications, patents, certifications, professionalHeadline, bio, industry, specialties, work, skills, religion, politicalView, interestedIn, relationshipStatus, hometown, favorites, followersCount, followingCount, username, locale, verified, timezone, likes, samlData"
+    }
+    
+    gigya.accounts.getAccountInfo({callback: getAccountInfoResponse });
+
+}
+
+function delay(n) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, n * 1000);
+    });
+}
+
+
+
+function getAccountInfoResponse(response) {
+    if (response.errorCode == 0) {
+        var firstName = response.profile.firstName;
+        var lastName = response.profile.lastName;
+        var UserType = response.data.UserType;
+        var UID = sessionStorage.getItem("SUID");
+        let DBName = "Users";
+        let Table = "Users_Info";
+        //var Data = response;
+        //alert(firstName);
+        var Data = JSON.parse(JSON.stringify(response));
+        
+        UpdateData(DBName, Table, Data, UID);
+        var elem = document.getElementById('ni');
+        if (typeof elem !== 'undefined' && elem !== null) {
+            document.getElementById('ni').onclick = function () {
+                Getdata('MyProfile.html', UID);
+            };
+        }
+        
+        //cancel();
+        
+        passwordreset();
+
+        //Getdata('MyProfile.html',UID);
+        //Delete(DBName, Table, UID).then(function (SUID) {
+            //Delete(DBName, Table, UID);
+            
+        //});
+        //ProcessDB(DBName, Table, Data, UID);
+        //alert('Profle Updated');
+        //Getdata('MyProfile.html');
+        
+       
+    }
+    else {
+        alert('Error :' + response.errorMessage);
+    }
+}
+
+
+function SignOut() {
+    
+    function printResponse(response) {
+        if (response.errorCode == 0) {
+
+            sessionStorage.clear()
+            //alert('Logged out');
+            window.location.href = 'index.html';
+
+
+        }
+        else {
+            alert('Error :' + response.errorMessage);
+        }
+    }
+    gigya.accounts.logout({ callback: printResponse });
+
+}
+
+function ForgotPass() {
+
+    load();
+    customLangParams = {
+
+        this_field_is_required: 'Please enter %fieldname'
+    };
+
+    gigya.accounts.showScreenSet({
+        screenSet: 'Online_Medical-RegistrationLogin',
+        startScreen: 'gigya-reset-password-screen',
+        customLang: customLangParams,
+        //containerID: 'divsignin'
+    });
+    
+}
+
+function passwordreset() {
+    
+    document.getElementById('passchangesuccess').onclick = function () {
+        SignOut();
+    };
+    //var elem = document.getElementById('passchangesuccess');
+    //alert(elem);
+    //if (typeof elem !== 'undefined' && elem !== null) {
+    //    document.getElementById('passchangesuccess').onclick = function () {
+    //        SignOut();
+    //    };
+    //}
+}
+
+function cancel() {
+
+    var elem = document.getElementById('PCancel');
+    if (typeof elem !== 'undefined' && elem !== null) {
+        //alert('clicked');
+        document.getElementById('PCancel').onclick = function () {
+            window.location = 'MyProfile.html';
+        };
+    }
+}
+
+
+function ChangePassScreen() {
+    load();
+    customLangParams = {
+
+        this_field_is_required: 'Please enter %fieldname'
+    };
+
+    gigya.accounts.showScreenSet({
+        screenSet: 'Online_Medical-ProfileUpdate',
+        startScreen: 'gigya-change-password-screen',
+        customLang: customLangParams,
+        //containerID: 'divsignup',
+        onAfterSubmit: passwordreset
+    });
+}
+
+
+function CheckLogin() {
+    //let flag;
+    var session_UID = sessionStorage.getItem("SUID");
+    
+    if (session_UID == null || typeof session_UID == "undefined") {
+        //flag = 0;
+        alert('Please Login to Proceed');
+        window.location  = 'index.html';
+
+    }
+    else {
+        //console.log(session_UID);
+        //window.location.href = page;
+        //flag = 1;
+    }
+
+    return session_UID;
+}
+
+function NewUserStore(response) {
+
+    let UID = response.UID;
+    let DBName = "Users";
+    let Table = "Users_Info";
+    ProcessDB(DBName, Table, response, UID);
+}
+
---
+++ assets/js/IndexedDB.js
@@ -0,0 +1,549 @@
+﻿function ProcessDB(DBName, Table, Data, Key) {
+    return new Promise(function (resolve) {
+        // check for IndexedDB support
+        if (!window.indexedDB) {
+            console.log("Your browser doesn't support IndexedDB");
+            return;
+        }
+        else {
+            
+            console.log("Your browser support IndexedDB");
+            
+
+            //alert(Key);
+            // open the CRM database with the version 1
+            var request =  indexedDB.open(DBName, 3);
+
+            // create the Contacts object store and indexes
+            request.onupgradeneeded = (event) => {
+
+                console.log("On_ProcessDB_Onupgrade");
+                var db = event.target.result;
+                if (!db.objectStoreNames.contains(Table)) {
+                    console.log("On_Insert_Store_not_exists_Creating");
+                    var store = db.createObjectStore(Table, { keyPath: Key }
+                    );
+                    //console.log("now here");
+                    // create an index on the email property
+                    store.createIndex('UID', 'UID', {
+                        unique: true
+                    });
+                    //store = txn.objectStore(Table);
+                }
+
+
+            }
+            // handle the error event
+            request.onerror = (event) => {
+                console.error('On_ProcessDB_onerror_Database error: ${event.target.errorCode}');
+            };
+
+            // handle the success event
+            request.onsuccess = (event) => {
+                
+                console.log("On_ProcessDB_Onsuccess");
+                
+                insert(event, Table, Data);
+            }
+
+
+        }
+
+      
+    });
+}
+
+
+function insert(event,Table,Data) {
+
+    var db = event.target.result;
+	var txn = event.target.transaction;
+	let store;
+    //var txn;
+    //const store = txn.objectStore(Table);
+      //else {
+        //console.log("On_Insert_Store_Exists");
+        txn = db.transaction(Table, 'readwrite');
+		store = txn.objectStore(Table);
+		
+	//}
+        let query = store.put(Data);
+           query.onsuccess = function (event) {
+            console.log("On_Insert_Success");
+        };
+
+        // handle the error case
+        query.onerror = function (event) {
+            
+            console.log("On_Insert_Error : " + event.target.errorCode);
+        };
+
+    // close the database once the 
+    // transaction completes
+    txn.oncomplete = function () {
+        db.close();
+    };
+
+}
+
+function getUID(DBName, Table, UID) {
+
+    return new Promise(function (resolve) {
+
+
+        const request = indexedDB.open(DBName);
+        // handle the success event
+        request.onsuccess = (event) => {
+
+            console.log("On_getUID_onsuccess");
+            getUIDChild(event, Table, UID,resolve);
+
+        };
+
+        request.onupgradeneeded = (event) => {
+
+            console.log("On_getUID_onupgrade");
+            getUIDChild(event, Table, UID,resolve);
+        }
+
+        request.onerror = (event) => {
+
+            console.log("On_getUID_onerror");
+        }
+
+
+    });
+
+}
+
+function getUIDChild(event, Table, UID, resolve) {
+    var SUID;
+    var SName;
+    let db = event.target.result;
+    var SOrgName;
+    var Orgid;
+	//var txn = event.target.transaction;
+    let txn = db.transaction(Table, 'readwrite');
+    let store = txn.objectStore(Table);
+    const index = store.index('UID');
+    // query by indexes
+    var query = index.get(UID);
+
+    // return the result object on success
+    query.onsuccess = function () {
+        console.log("On_getUIDChild_onsuccess");
+        SUID = query.result.UID;
+        //SName = query.result.profile.firstName + " " + query.result.profile.lastName;
+        SName = query.result.profile.firstName;
+        Sprovider = query.result.provider;
+        
+        if (sessionStorage.getItem("flag") == 'b2b') {
+            SOrgName = query.result.groups.organizations[0].orgName;
+            SUID = SUID + "," + SName + "," + Sprovider + "," + SOrgName;
+            Orgid = query.result.groups.organizations[0].orgId;
+            var roles = query.result.groups.organizations[0].roles;
+            sessionStorage.setItem("OrgID", Orgid);
+            sessionStorage.setItem("roles", roles);
+            roles.forEach(function (roleid) {
+                role = localStorage.getItem(roleid);
+                if (role == "Delegated Admin") {
+                    sessionStorage.setItem("DACheck", "Yes");
+                    
+                }
+
+                }
+            )}
+      
+        else
+        {
+            //SOrgName = query.result.groups.organizations[0].orgName;
+            SUID = SUID + "," + SName + "," + Sprovider;
+            //alert(SUID);
+        }
+        console.table(query.result); // result objects
+        return resolve(SUID);
+
+    };
+
+    query.onerror = (event) => {
+        console.log("On_getUIDChild_onerror : " + event.target.errorCode);
+    };
+
+    // close the database connection
+    txn.oncomplete = function () {
+        db.close();
+        //return SUID;
+    }
+}
+
+function UpdateData(DBName, Table, Data, UID) {
+    var request = indexedDB.open(DBName);
+        // handle the success event
+    request.onsuccess = (event) => {
+        console.log("On_UpdateData_onsuccess");
+            UpdateDataChild(event, Table, Data);
+        }
+        request.onerror = (event) => {
+            console.log("On_UpdateData_onerror : " + event.target.errorCode);
+        };
+    request.onupgradeneeded = (event) => {
+
+        console.log("On_UpdateData_onupgrade");
+        UpdateDataChild(event, Table, Data);
+    }
+}
+            
+function UpdateDataChild(event,Table, Data) {
+
+    let db = event.target.result;
+	//var txn = event.target.transaction;
+    var transaction = db.transaction(Table, "readwrite");
+    var store = transaction.objectStore(Table);
+    //alert("ni" + Data);
+
+    let query = store.put(Data);
+    console.log("On_UpdateDataChild_Data_Success");
+    query.onsuccess = function (event) {
+
+
+
+    }
+
+    query.onerror = function (event) {
+
+        console.error("On_UpdateDataChild_onerror : " + event.target.errorCode);
+
+    }
+
+    // close the database connection
+    transaction.oncomplete = function () {
+        db.close();
+        //return SUID;
+    }
+
+}
+
+
+
+
+function Delete(DBName, Table, UID) {
+    
+
+    return new Promise(function (resolve) {
+        var request = indexedDB.open(DBName);
+        request.onsuccess = (event) => {
+
+            DeleteChild(event, UID);
+        }
+
+        request.onupgradeneeded = (event) => {
+
+            console.log("On_Delete_Onupgrade");
+            DeleteChild(event, UID);
+        }
+        request.onerror = (event) => {
+
+            console.log("On_Delete_Onerror");
+        }
+    });
+}
+
+function DeleteChild(event,UID) {
+
+
+    let db = event.target.result;
+	//var txn = event.target.transaction;
+    txn = db.transaction(Table, "readwrite");
+    objectStore = txn.objectStore(Table);
+    request = objectStore.delete(UID);
+    console.log("deleted UID : " + UID);
+    request.onsuccess = function (evt) {
+        console.log("On_DeleteChild_onsuccess");
+        
+    };
+
+    request.onerror = function (evt) {
+        console.log("On_DeleteChild_onerror : " + event.target.errorCode);
+
+    };
+	// close the database connection
+    txn.oncomplete = function () {
+        db.close();
+    };
+
+}
+
+function getRecords(DBName, Table, UID) {
+ 
+    const request = indexedDB.open(DBName);
+
+    // handle the success event
+    request.onsuccess = (event) => {
+        console.log("On_getRecords_onsuccess");
+        getRecorsChild(event,Table, UID);
+
+    };
+
+    request.onupgradeneeded = (event) => {
+
+        console.log("On_getRecords_onupgrade");
+        getRecorsChild(event,Table, UID);
+
+    }
+
+    request.onerror = (event) => {
+
+        console.log("On_getRecords_onerror");
+        
+
+    }
+
+}
+
+function getRecorsChild(event,Table,UID) {
+
+    let db = event.target.result;
+	//var txn= event.target.transaction;
+    var txn = db.transaction(Table, 'readonly');
+    var store = txn.objectStore(Table);
+    const index = store.index('UID');
+    // query by indexes
+    let query = index.get(UID);
+
+    // return the result object on success
+    query.onsuccess = (event) => {
+
+        console.log("On_getRecordsChild_onsuccess");
+        var result = event.target.result;
+        //alert(result.groups.organizations[0].orgName);
+        document.getElementById('UID').value = result.UID;
+        document.getElementById('email').value = result.profile.email;
+        document.getElementById('firstName').value = result.profile.firstName;
+        document.getElementById('lastName').value = result.profile.lastName;
+        var role;
+        var rolenames='';
+        if (sessionStorage.getItem("flag") == 'b2b') {
+            document.getElementById('Organization').value = result.groups.organizations[0].orgName;
+            document.getElementById('Department').value = result.groups.organizations[0].department;
+            document.getElementById('Job').value = result.groups.organizations[0].job;
+            var roles = result.groups.organizations[0].roles;
+
+            roles.forEach(function (roleid) {
+                role = localStorage.getItem(roleid);
+                role = role + "\n";
+                rolenames = rolenames + role;
+            });
+            document.getElementById('Roles').value = rolenames;
+            
+
+            
+            //document.getElementById('Roles').value = result.groups.organizations[0].roles;
+        }
+            console.dir(result);
+        
+    };
+
+    query.onerror = (event) => {
+        console.log("On_getRecordsChild : " + event.target.errorCode);
+    };
+
+    // close the database connection
+    txn.oncomplete = function () {
+        db.close();
+    };
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+        
+//(function () {
+
+    
+
+//    // open the CRM database with the version 1
+//    const request = indexedDB.open('UserInfo', 1);
+
+//    // create the Contacts object store and indexes
+//    request.onupgradeneeded = (event) => {
+//        let db = event.target.result;
+
+//        // create the Contacts object store 
+//        // with auto-increment id
+//        let store = db.createObjectStore('Users', {
+//            autoIncrement: false
+//        });
+
+//        // create an index on the email property
+//        let index = store.createIndex('UID', 'UID', {
+//            unique: true
+//        });
+//    };
+
+//    // handle the error event
+//    request.onerror = (event) => {
+//        console.error(`Database error: ${event.target.errorCode}`);
+//    };
+
+//    // handle the success event
+//    request.onsuccess = (event) => {
+//        const db = event.target.result;
+
+//        // insert contacts
+//        // insertContact(db, {
+//        //     email: 'john.doe@outlook.com',
+//        //     firstName: 'John',
+//        //     lastName: 'Doe'
+//        // });
+
+//        // insertContact(db, {
+//        //     email: 'jane.doe@gmail.com',
+//        //     firstName: 'Jane',
+//        //     lastName: 'Doe'
+//        // });
+
+
+//        // get contact by id 1
+//        // getContactById(db, 1);
+
+
+//        // get contact by email
+//        // getContactByEmail(db, 'jane.doe@gmail.com');
+
+//        // get all contacts
+//        // getAllContacts(db);
+
+//        deleteContact(db, 1);
+
+//    };
+
+    //function insertContact(DBName, Table,Data) {
+    //    // create a new transaction
+    //    const txn = db.transaction(Table, 'readwrite');
+
+    //    // get the Contacts object store
+    //    const store = txn.objectStore(Table);
+    //    //
+    //    let query = store.put(Table);
+
+    //    // handle success case
+    //    query.onsuccess = function (event) {
+    //        console.log(event);
+    //    };
+
+    //    // handle the error case
+    //    query.onerror = function (event) {
+    //        console.log(event.target.errorCode);
+    //    }
+
+    //    // close the database once the 
+    //    // transaction completes
+    //    txn.oncomplete = function () {
+    //        db.close();
+    //    };
+    //}
+
+
+//    function getContactById(db, id) {
+//        const txn = db.transaction('Contacts', 'readonly');
+//        const store = txn.objectStore('Contacts');
+
+//        let query = store.get(id);
+
+//        query.onsuccess = (event) => {
+//            if (!event.target.result) {
+//                console.log(`The contact with ${id} not found`);
+//            } else {
+//                console.table(event.target.result);
+//            }
+//        };
+
+//        query.onerror = (event) => {
+//            console.log(event.target.errorCode);
+//        }
+
+//        txn.oncomplete = function () {
+//            db.close();
+//        };
+//    };
+
+//    function getContactByEmail(db, email) {
+//        const txn = db.transaction('Contacts', 'readonly');
+//        const store = txn.objectStore('Contacts');
+
+//        // get the index from the Object Store
+//        const index = store.index('email');
+//        // query by indexes
+//        let query = index.get(email);
+
+//        // return the result object on success
+//        query.onsuccess = (event) => {
+//            console.table(query.result); // result objects
+//        };
+
+//        query.onerror = (event) => {
+//            console.log(event.target.errorCode);
+//        }
+
+//        // close the database connection
+//        txn.oncomplete = function () {
+//            db.close();
+//        };
+//    }
+
+//    function getAllContacts(db) {
+//        const txn = db.transaction('Contacts', "readonly");
+//        const objectStore = txn.objectStore('Contacts');
+
+//        objectStore.openCursor().onsuccess = (event) => {
+//            let cursor = event.target.result;
+//            if (cursor) {
+//                let contact = cursor.value;
+//                console.log(contact);
+//                // continue next record
+//                cursor.continue();
+//            }
+//        };
+//        // close the database connection
+//        txn.oncomplete = function () {
+//            db.close();
+//        };
+//    }
+
+
+//    function deleteContact(db, id) {
+//        // create a new transaction
+//        const txn = db.transaction('Contacts', 'readwrite');
+
+//        // get the Contacts object store
+//        const store = txn.objectStore('Contacts');
+//        //
+//        let query = store.delete(id);
+
+//        // handle the success case
+//        query.onsuccess = function (event) {
+//            console.log(event);
+//        };
+
+//        // handle the error case
+//        query.onerror = function (event) {
+//            console.log(event.target.errorCode);
+//        }
+
+//        // close the database once the 
+//        // transaction completes
+//        txn.oncomplete = function () {
+//            db.close();
+//        };
+
+//    }
+//})();
